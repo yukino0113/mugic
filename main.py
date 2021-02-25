5ddd6f2a789f0d3b93b2343ebe0f51096c8a9970
yukino0113 <31920929+yukino0113@users.noreply.github.com>
2021-02-25 22:15:24 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import QUIT
+
+# game screen settings
+pygame.init()
+windowSize = 1200, 600
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Mugic Ver.alpla")
+
+def game_menu():
+
+    def load_menu_pic():
+
+
